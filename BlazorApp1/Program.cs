3a88c53435bbc7002c4be37f01c5a5a43a3e70a0
John-1-1-1 <52410048+John-1-1-1@users.noreply.github.com>
2023-04-10 00:38:41 +0700

    Add Hub

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -2,19 +2,19 @@ using BlazorApp1;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using BlazorApp1.Data;
+using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
- 
 // получаем строку подключения из файла конфигурации
 string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
-
 // добавляем контекст ApplicationContext в качестве сервиса в приложение
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connection));
 
@@ -27,16 +27,11 @@ if (!app.Environment.IsDevelopment()) {
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
-// получение данных
+app.MapHub<BlazorChatSampleHub>(BlazorChatSampleHub.HubUrl);
+// получение данных TODO:TESTS
 app.MapGet("/data", (ApplicationContext db) => db.Users.ToList());
-
 app.Run();
